af678e54107825ff9ff9582a890be4d6a770b90e
Bernardo-JGD <guzman.diaz.jb@gmail.com>
2022-08-02 15:34:52 -0500

    Avanzando en expresiones regulares

--- ExpresionesRegulares/Program.cs
+++ ExpresionesRegulares/Program.cs
@@ -16,22 +16,22 @@ namespace ExpresionesRegulares
     {
         static void Main(string[] args)
         {
-            encontrarJ();
-
+            //encontrarJ();
+            practicaNombres();
             Console.ReadLine();
         }
 
         static void encontrarJ()
         {
-            string frase = "Mi nombre es Juan y mi n° de tfno es (+34)123-45-67 y mi código postal es 29679";
-
+            string frase = "Mi nombre es Juan y mi n° de tfno es (+52)123-45-67 y mi código postal es 29679";
+            string fraseWeb = "";
             string patronJ = "[J]";//Todas las concidencias con J
             string patronNumero = @"\d";//Todas las coincidencias con un número
             string patronNumeros = @"\d{3}";//Todas las coincidencias con un tres números
+            string patronO = @"\+34|\+52";//Coincide con una opción o con la otra
+
             Regex miRegex = new Regex(patronJ);
-
             MatchCollection coincidencia = miRegex.Matches(frase);
-
             if (coincidencia.Count>0)
             {
                 Console.WriteLine("Se encontró una J");
@@ -62,6 +62,25 @@ namespace ExpresionesRegulares
             {
                 Console.WriteLine("No encontró un grupo de 3 números");
             }
+
+            miRegex = new Regex(patronO);
+            coincidencia = miRegex.Matches(frase);
+            if (coincidencia.Count > 0)
+            {
+                Console.WriteLine("Hay números de Méxivo");
+            }
+            else
+            {
+                Console.WriteLine("No hay números de México");
+            }
         }
+        
+        static void practicaNombres()
+        {
+            string nombre = "Jose Bernardo Guzmán Diaz";
+            string nombre2 = "";
+            Console.WriteLine(nombre2.Length);
+        }
+    
     }
 }

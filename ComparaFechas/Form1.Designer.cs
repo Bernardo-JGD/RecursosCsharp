c86accb9fdbb4aa4fc6c30210bdc348933e6ecca
Bernardo-JGD <guzman.diaz.jb@gmail.com>
2022-08-16 17:30:41 -0500

    Cree un comparador de fechas para solucionar validación en el proyecto Cuenpi

---
+++ ComparaFechas/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ComparaFechas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selecFecha2 = new System.Windows.Forms.DateTimePicker();
+            this.selecFecha1 = new System.Windows.Forms.DateTimePicker();
+            this.lbResultado = new System.Windows.Forms.Label();
+            this.btnComparar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // selecFecha2
+            // 
+            this.selecFecha2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selecFecha2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.selecFecha2.Location = new System.Drawing.Point(52, 145);
+            this.selecFecha2.Name = "selecFecha2";
+            this.selecFecha2.Size = new System.Drawing.Size(137, 35);
+            this.selecFecha2.TabIndex = 0;
+            // 
+            // selecFecha1
+            // 
+            this.selecFecha1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selecFecha1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.selecFecha1.Location = new System.Drawing.Point(52, 67);
+            this.selecFecha1.Name = "selecFecha1";
+            this.selecFecha1.Size = new System.Drawing.Size(137, 35);
+            this.selecFecha1.TabIndex = 1;
+            this.selecFecha1.Value = new System.DateTime(2022, 8, 16, 15, 40, 16, 0);
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.AutoSize = true;
+            this.lbResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResultado.Location = new System.Drawing.Point(220, 145);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(122, 29);
+            this.lbResultado.TabIndex = 2;
+            this.lbResultado.Text = "Resultado";
+            // 
+            // btnComparar
+            // 
+            this.btnComparar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnComparar.Location = new System.Drawing.Point(214, 67);
+            this.btnComparar.Name = "btnComparar";
+            this.btnComparar.Size = new System.Drawing.Size(141, 35);
+            this.btnComparar.TabIndex = 3;
+            this.btnComparar.Text = "Comparar";
+            this.btnComparar.UseVisualStyleBackColor = true;
+            this.btnComparar.Click += new System.EventHandler(this.btnComparar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Fecha 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Fecha 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 231);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnComparar);
+            this.Controls.Add(this.lbResultado);
+            this.Controls.Add(this.selecFecha1);
+            this.Controls.Add(this.selecFecha2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker selecFecha2;
+        private System.Windows.Forms.DateTimePicker selecFecha1;
+        private System.Windows.Forms.Label lbResultado;
+        private System.Windows.Forms.Button btnComparar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ComparaFechas/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComparaFechas
+{
+    /* Recursos 
+     * https://www.iteramos.com/pregunta/10760/como-comparar-solo-la-fecha-sin-la-hora-en-los-tipos-datetime-en-linq-to-sql-con-entity-framework
+     * https://www.delftstack.com/es/howto/csharp/how-to-convert-a-string-to-datetime-in-csharp/
+     * https://es.acervolima.com/metodo-datetime-compare-en-c/
+     
+     */
+    public partial class Form1 : Form
+    {
+        int resultado;
+        string fecha1;
+        string fecha2;
+        public Form1()
+        {
+            InitializeComponent();
+            compararFechas();
+        }
+
+
+        private void btnComparar_Click(object sender, EventArgs e)
+        {
+            compararFechas();
+        }
+
+        private void compararFechas()
+        {
+            //fecha1 = selecFecha1.Value.ToString("yyyy/dd/MM");
+           // fecha2 = selecFecha2.Value.ToString("yyyy/dd/MM");
+            resultado = DateTime.Compare(selecFecha1.Value.Date, selecFecha2.Value.Date);
+            if (resultado<0)
+            {
+                lbResultado.Text = "Fecha 1 es menor que fecha 2";
+            }
+            else
+            {
+
+                if (resultado == 0)
+                {
+                    lbResultado.Text = "Ambas fechas son iguales";
+                }
+                else
+                {
+                    lbResultado.Text = "La fecha 1 es mayor que la fecha 2";
+                }
+                
+            }
+        }
+
+        
+    }
+}
